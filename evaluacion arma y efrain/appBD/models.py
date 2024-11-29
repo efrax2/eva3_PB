720ee2898e21ea8b1f35dbe48460b9956846baa2
KatnipCrowley <nickboxguns@gmail.com>
2024-11-29 18:49:33 -0300

    arreglado

--- evaluacion arma y efrain/appBD/models.py
+++ evaluacion arma y efrain/appBD/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class alumnos(models.Model):
-    ALUMRUT=models.CharField(max_length=12, unique=True)
+    ALUMRUT=models.CharField(max_length=12, primary_key=True, default=1)
     ALUMNOMBRE=models.CharField(max_length=15)
     ALUMAPATERNO=models.CharField( max_length=15)
     ALUMAMATERNO=models.CharField(max_length=15)
@@ -11,28 +12,27 @@ class alumnos(models.Model):
     ALUMNFONO=models.CharField(max_length=15)
 
 class tipo_cursos(models.Model):
-    TIPCURCODIGO=models.SmallIntegerField(max_length=6, unique=True)
+    TIPCURCODIGO=models.IntegerField(max_length=6, primary_key=True, default=1)
     TIPCURNOMBRE=models.CharField(max_length=15) 
     TIPCURVALOR=models.IntegerField(max_length=11)
 
 
 class ciudades(models.Model):
-    CIUCODIGO=models.SmallIntegerField(max_length=6, unique=True)
+    CIUCODIGO=models.IntegerField(max_length=6, primary_key=True)
     CIUNOMBRE=models.CharField(max_length=20)
     
 class sucursales(models.Model):
-    SUCCODIGO=models.SmallIntegerField(max_length=6, unique=True)
-    CIUCODIGO=models.ForeignKey(ciudades,on_delete=models.CASCADE)
+    SUCCODIGO=models.IntegerField(max_length=6, primary_key=True)
+    CIUCODIGO=models.ForeignKey(ciudades, on_delete=models.CASCADE, default=1)
     SUCNOMBRE=models.CharField(max_length=20)
 
 class matriculas(models.Model):
-    MATNUMERO=models.SmallIntegerField(max_length=11, unique=True)
-    TIPCURCODIGO=models.ForeignKey(tipo_cursos,on_delete=models.CASCADE)
-    ALUMRUT=models.ForeignKey(alumnos,on_delete=models.CASCADE)
-    SUCCODIGO=models.ForeignKey(sucursales,on_delete=models.CASCADE)
-    MATFECHA=models.DateField()
-
+    MATNUMERO=models.IntegerField(max_length=11, primary_key=True, default=1)
+    TIPCURCODIGO=models.ForeignKey(tipo_cursos,on_delete=models.CASCADE, default=1)
+    ALUMRUT=models.ForeignKey(alumnos, on_delete=models.CASCADE, default=1)
+    SUCCODIGO=models.ForeignKey(sucursales,on_delete=models.CASCADE, default=1)
+    MATFECHA=models.DateField(default=timezone.now)
 
 class usuarios(models.Model):
-    USULOGIN=models.CharField(max_length=15)
+    USULOGIN=models.CharField(max_length=15, primary_key=True, default=1)
     USUPASSWORD=models.CharField(max_length=15)
